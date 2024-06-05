0c85c27a6a135c2fde6c3d06a3c5a7698258e576
YonWaiHung <waihung.yon@beans.com.my>
2024-06-05 16:57:43 +0800

    developer file format

--- developers.php
+++ developers.php
@@ -1,35 +1,35 @@
 <?php
 include("database.php");
 
-$db= $conn;
-$tableName="travel";
-$columns= ['travel_id', 'travel_photo','travel_name','travel_location','travel_date', 'travel_note'];
+$db = $conn;
+$tableName = "travel";
+$columns = ['travel_id', 'travel_photo', 'travel_name', 'travel_location', 'travel_date', 'travel_note'];
 $fetchData = fetch_data($db, $tableName, $columns);
 
-function fetch_data($db, $tableName, $columns){
- if(empty($db)){
-  $msg= "Database connection error";
- }elseif (empty($columns) || !is_array($columns)) {
-  $msg="columns Name must be defined in an indexed array";
- }elseif(empty($tableName)){
-   $msg= "Table Name is empty";
-}else{
+function fetch_data($db, $tableName, $columns)
+{
+  if (empty($db)) {
+    $msg = "Database connection error";
+  } elseif (empty($columns) || !is_array($columns)) {
+    $msg = "columns Name must be defined in an indexed array";
+  } elseif (empty($tableName)) {
+    $msg = "Table Name is empty";
+  } else {
 
-$columnName = implode(", ", $columns);
-$query = "SELECT ".$columnName." FROM $tableName"." ORDER BY travel_id DESC";
-$result = $db->query($query);
+    $columnName = implode(", ", $columns);
+    $query = "SELECT " . $columnName . " FROM $tableName" . " ORDER BY travel_id DESC";
+    $result = $db->query($query);
 
-if($result== true){ 
- if ($result->num_rows > 0) {
-    $row= mysqli_fetch_all($result, MYSQLI_ASSOC);
-    $msg= $row;
- } else {
-    $msg= "No Data Found"; 
- }
-}else{
-  $msg= mysqli_error($db);
+    if ($result == true) {
+      if ($result->num_rows > 0) {
+        $row = mysqli_fetch_all($result, MYSQLI_ASSOC);
+        $msg = $row;
+      } else {
+        $msg = "No Data Found";
+      }
+    } else {
+      $msg = mysqli_error($db);
+    }
+  }
+  return $msg;
 }
-}
-return $msg;
-}
-?>
